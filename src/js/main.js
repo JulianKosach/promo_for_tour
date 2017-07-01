36d1b8f66e389359aea9dfed3695b99683099187
JulianKosach <juliankosach@gmail.com>
2017-07-01 16:31:16 +0300

    adaptive 1screen + menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,24 @@ function scrollTo(top) {
 
 $(document).ready(function() {
 
+	$('.menu-btn').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.menu-list').toggleClass('active');
+	});
+
+	$('.close-menu').on('click', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$('.menu-list').removeClass('active');
+	});
+
+
+
 	$('.go-to').on('click', function(e){
 		e.preventDefault();
 		e.stopPropagation();
+		$('.menu-list').removeClass('active');
 		var sel = $(this).attr('data-go-to');
 		var top = 0;
 		if (sel == '#s2') {
