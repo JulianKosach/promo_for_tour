4acc00768334df08ff81f04e63a3d9b1ebe1270b
JulianKosach <juliankosach@gmail.com>
2017-07-04 13:16:52 +0300

    adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -78,7 +78,7 @@ $(document).ready(function() {
 			maxSlides: 1,
 			moveSlides: 1,
 			slideMargin: 0,
-			touchEnabled: true,
+			touchEnabled: false,
 			auto: false,
 			onSlideBefore: function($slideElement, oldIndex, newIndex) {
 
